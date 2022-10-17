8ed263cf94ae11be6f70e2298c035c0c779c9698
Kbgjtn <Kambingjan31@gmail.com>
2022-10-17 20:42:44 +0000

    add comment

---
+++ src/listDataEntry/App.java
@@ -0,0 +1,28 @@
+// use local package
+package listDataEntry;
+
+// import listDataEntry local module
+import listDataEntry.*;
+
+/**
+ * Session 3 | Create a program that allows to receive multiples entry data,
+ * shows the data from the list, and delete from the list as following
+ * 
+ * Lecturer Info
+ * 
+ * @lecturer_name: ANNISA ISTIQOMAH ARRAHMAH, S.T., M.T.
+ * @lecturer_id: D6331
+ * @Class: LA75-LEC | Object Oriented Programming
+ * 
+ * 
+ *         Student Info
+ * 
+ * @member: Naufal Daffa Ryquelme[2502012312] & Irham Zhafran Atmoko[2502007880]
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // call Features methode fn useFeatures() for run program
+        Features.useFeatures();
+    }
+}
