1af973aa62123d904bfab4abeea646e9882e1933
Kbgjtn <Kambingjan31@gmail.com>
2022-10-17 20:12:54 +0000

    List Data Entry

---
+++ src/listDataEntry/App.java
@@ -0,0 +1,26 @@
+package listDataEntry;
+
+import listDataEntry.*;
+
+/**
+ * Session 3 | Create a program that allows to receive multiples entry data,
+ * shows the data from the list, and delete from the list as following
+ * 
+ * Lecturer Info
+ * 
+ * @lecturer_name: ANNISA ISTIQOMAH ARRAHMAH, S.T., M.T.
+ * @lecturer_id: D6331
+ * @Class: LA75-LEC | Object Oriented Programming
+ * 
+ * 
+ *         Student Info
+ * 
+ * @member: Naufal Daffa Ryquelme & Irham Zhafran
+ * @NIM: 2502012312
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Features.useFeatures();
+    }
+}
